90d2732bba5f3534bca6f2dad2463766f613229f
Phophi96 <140660612+Phophi96@users.noreply.github.com>
2023-09-08 23:34:30 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+/**
+ * main -  Entry
+ * Return: (0)
+ */
+int main(void)
+{
+	char letter = 'a';
+  char LETTER = 'A'
 
+	while (letter <= 'z')
+	{
+	putchar(letter);
+  letter++;
+	}
+  while (LETTER <= 'Z')
+	{
+  putchar(LETTER);
+  LETTER++;
+	}
+	putchar('\n');
+	return (0);
+}
