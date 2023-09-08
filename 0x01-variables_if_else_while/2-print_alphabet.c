fdf5b884ffd5fa3a280c635b6767138b7bf9dce4
Phophi96 <140660612+Phophi96@users.noreply.github.com>
2023-09-08 23:02:05 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+/**
+ * main -  Entry
+ * Return: (0)
+ */
+int main(void) {
+    char letter = 'a';
 
+    while (letter <= 'z') {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n'); // Print a newline character
+
+    return 0;
+}
