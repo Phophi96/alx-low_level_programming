a57cb961f89a5afc69970487f57f1ed46006eebd
Phophi96 <margaphophi@gmail.com>
2023-09-27 01:31:19 -0700

    Recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a number
+ * @z: number to return the factorial from
+ *
+ * Return: factorial of z
+ */
+int factorial(int z)
+{
+	if (z < 0)
+		return (-1);
+	if (z == 0)
+		return (1);
+	return (z * factorial(z - 1));
+}
